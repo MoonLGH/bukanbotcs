2732d765b3b4baec09b950979a59013285a3c938
MoonLGH <MoonLMC1212@gmail.com>
2021-03-15 17:53:58 +0700

    test

--- command/setbotactivity.js
+++ command/setbotactivity.js
@@ -1,21 +1,21 @@
 exports.execute = function (msg, command, args, client, D, perm, color) {
-    let input = args.join(" ").match(/\w+|('|")([^"]|[^'])+('|")/g);
-    let string = input[0].replace(/["']/g, "")
+    let input = args.match(/"(.*?)"/g)
+    let string = input[0]
 
     let type = input[1]
     console.log(input)
     if (!type) type = "PLAYING"
     if (type == "PLAYING") {
         type = "PLAYING"
-    }else if (type == "STREAMING"){
+    } else if (type == "STREAMING") {
         type = "STREAMING"
-    }else if (type == "LISTENING"){
+    } else if (type == "LISTENING") {
         type = "LISTENING"
-    }else if (type == "WATCHING"){
+    } else if (type == "WATCHING") {
         type = "WATCHING"
-    }else if(type == "COMPETING"){
+    } else if (type == "COMPETING") {
         type = "COMPETING"
-    }else{
+    } else {
         return msg.channel.send("Tipe yang kamu masukan tidak valid!. \n Tipe yang valid adalah: \n-PLAYING \n-STREAMING\n-LISTENING\n-COMPETING")
     }
 
@@ -25,5 +25,5 @@ exports.execute = function (msg, command, args, client, D, perm, color) {
         }
     )
 
-    msg.channel.send(`New Activity Setted!. \n${string} With ${type} Type of Activity`)
+    msg.channel.send(`New Activity Setted!. \n \`${string}\` With \`${type}\` As The Type of Activity`)
 }
