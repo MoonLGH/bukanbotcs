65e08c1f1393b83773371e9b57a92c9887e28844
MoonLGH <MoonLMC1212@gmail.com>
2021-03-21 12:16:37 +0700

    img

--- command/ddcommand/home.js
+++ command/ddcommand/home.js
@@ -18,19 +18,83 @@ let list = []
         let chapter = $(this).find("div > div > div > div > div.plyepisode > a").text()
         let id = $(this).find("div > div > a").attr("href").replace(config.url,"")
         let chapterid = $(this).find("div > div > div > div > div.plyepisode > a").attr("href").replace(config.url,"")
-        list.push({name,type,chapter,id,chapterid})
+        let imageurl = $(this).find("div > div > a > div > img").attr("src")
+        list.push({name,type,chapter,id,chapterid,imageurl})
     })
 
-    const send = new D.MessageEmbed()
-    .setTitle('DoujinDesu Home')
-    .setAuthor('Bukan Cleansound', 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-    .setTimestamp()
-    .setFooter('Bukan Cleansound', 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-    .addField(`${list[0].name}`, `Nama : ${list[0].name}\n ID : ${list[0].id}\n Tipe : ${list[0].type}\n Chapter : ${list[0].chapter}\n Chapter ID : ${list[0].chapterid}`, true)
-    .addField(`${list[1].name}`, `Nama : ${list[1].name}\n ID : ${list[1].id}\n Tipe : ${list[1].type}\n Chapter : ${list[1].chapter}\n Chapter ID : ${list[1].chapterid}`, true)
-    .addField(`${list[2].name}`, `Nama : ${list[2].name}\n ID : ${list[2].id}\n Tipe : ${list[2].type}\n Chapter : ${list[2].chapter}\n Chapter ID : ${list[2].chapterid}`, true)
-    .addField(`${list[3].name}`, `Nama : ${list[3].name}\n ID : ${list[3].id}\n Tipe : ${list[3].type}\n Chapter : ${list[3].chapter}\n Chapter ID : ${list[3].chapterid}`, true)
-    .addField(`${list[4].name}`, `Nama : ${list[4].name}\n ID : ${list[4].id}\n Tipe : ${list[4].type}\n Chapter : ${list[4].chapter}\n Chapter ID : ${list[4].chapterid}`, true)
+    let index = 0
+    let embed = new D.MessageEmbed()
+        .setAuthor(list[index].name, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setTimestamp()
+        .addField(`Nama`, list[index].name)
+        .addField(`ID `, list[index].id)
+        .addField(`Tipe`, list[index].type)
+        .addField(`Highlited Chapter`, list[index].chapter)
+        .addField(`Highlited Chapter ID`, list[index].chapterid)
+        .setImage(list[index].imageurl)
+        .setFooter(`${index+1} / ${list.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setColor(color);
 
-    msg.channel.send(send)
+    const embedsend = await msg.channel.send(embed)
+    await embedsend.react("🗑️")
+    await embedsend.react("⬅");
+    await embedsend.react("➡");
+
+    const deleteFilter = (reaction, user) =>
+        reaction.emoji.name === `🗑️` && user.id === msg.author.id;
+
+    const backwardsFilter = (reaction, user) =>
+        reaction.emoji.name === `⬅` && user.id === msg.author.id;
+
+    const forwardsFilter = (reaction, user) =>
+        reaction.emoji.name === `➡` && user.id === msg.author.id;
+
+        const backwards = embedsend.createReactionCollector(backwardsFilter);
+        const deletes = embedsend.createReactionCollector(deleteFilter);
+        const forwards = embedsend.createReactionCollector(forwardsFilter);
+        
+    deletes.on("collect", async f => {
+        embedsend.delete();
+    })
+
+    forwards.on("collect",async f =>{
+        if(index <= list.length){
+            index += 1
+        }
+        console.log(index)
+        embed = new D.MessageEmbed()
+        .setAuthor(list[index].name, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setTimestamp()
+        .addField(`Nama`, list[index].name)
+        .addField(`ID `, list[index].id)
+        .addField(`Tipe`, list[index].type)
+        .addField(`Highlited Chapter`, list[index].chapter)
+        .addField(`Highlited Chapter ID`, list[index].chapterid)
+        .setImage(list[index].imageurl)
+        .setFooter(`${index+1} / ${list.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setColor(color);
+
+        embedsend.edit(embed)
+    })
+    
+    backwards.on("collect",async f =>{
+        if(index >= 1){
+            index -= 1
+        }
+        embed = new D.MessageEmbed()
+        .setAuthor(list[index].name, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setTimestamp()
+        .addField(`Nama`, list[index].name)
+        .addField(`ID `, list[index].id)
+        .addField(`Tipe`, list[index].type)
+        .addField(`Highlited Chapter`, list[index].chapter)
+        .addField(`Highlited Chapter ID`, list[index].chapterid)
+        .setImage(list[index].imageurl)
+        .setFooter(`${index+1} / ${list.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setColor(color);
+
+        console.log(index)
+        embedsend.edit(embed)
+    })
+
 }
