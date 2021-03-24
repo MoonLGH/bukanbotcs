f98ea43fc3b37a35076b11c74390901b83115e9c
MoonLGH <MoonLMC1212@gmail.com>
2021-03-24 18:49:14 +0700

    filename

--- command/general/ytdl.js
+++ command/general/ytdl.js
@@ -3,9 +3,7 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
     if (ytdl.validateURL(args[0]) === false) return msg.channel.send("Put An Valid Youtube Link")
     let info = await ytdl.getBasicInfo(args[0])
 
-    let fileOptions = {
-        filename: info.videoDetails.title.replaceAll("[^a-zA-Z0-9\\.\\-]", "_"),
-    };
+        let filename = info.videoDetails.title.replaceAll("[^a-zA-Z0-9\\.\\-]", "_")
     let bufs = [];
     let stream = ytdl(args[0], {
         filter: format => format.container === 'mp4'
@@ -18,7 +16,7 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
         msg.channel.send("Download Sucsess", {
             files: [{
                 attachment: vid,
-                name: info.videoDetails.title.replaceAll("[^a-zA-Z0-9\\.\\-]", "_")
+                name: filename
             }]
         })
     })
