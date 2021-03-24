396c1af73cfd8826235c0f36b5afb7a2003d787a
MoonLGH <MoonLMC1212@gmail.com>
2021-03-24 14:01:45 +0700

    fixed handler

--- ext_module/commandhandler.js
+++ ext_module/commandhandler.js
@@ -10,28 +10,35 @@ let perm = require("./premission.js")
 exports.execute = function (msg, command, args, client, D) {
     let premission = perm.premission(msg, D)
 
-    if (command == "botownerauthority") {
+    if (command == "botownerauthority" || command == "ownerauthority" || command == "ownercommand" || command == "owner") {
         if (msg.author.id !== config.MoonLID) {
             msg.channel.send("Kamu Bukan <@" + config.MoonLID + ">")
         } else {
-            let command = args.shift().toLowerCase();
-            if (fs.existsSync(`./command/owner/${command}` + ".js") == true) {
-                require(`../command/owner/${command}`).execute(msg, command, args, client, D, premission, color, cd)
+
+            if (alias(command) !== null) {
+                cmd = alias(command)
+                if (fs.existsSync(`./command/owner/${cmd}` + ".js") == true) {
+                    require(`../command/owner/${cmd}`).execute(msg, command, args, client, D, premission, color, cd)
+                }
+            } else if (fs.existsSync(`./command/owner/${command}` + ".js") == true) {
+                cmd = command
+                require(`../command/owner/${cmd}`).execute(msg, command, args, client, D, premission, color, cd)
             }
         }
     } else if (command == "nh") {
         let command = args.shift().toLowerCase();
         hhandler.nh(msg, command, args, client, D)
-    }
-    if (command == "dd" || command == "doudesu" || command == "doujindesu") {
+    } else if (command == "dd" || command == "doudesu" || command == "doujindesu") {
         let command = args.shift().toLowerCase();
         hhandler.Doudesu(msg, command, args, client, D)
     } else {
         let cmd
-        if(alias(command) !== null){
+        if (alias(command) !== null) {
             cmd = alias(command)
-            require(`../command/general/${cmd}`).execute(msg, command, args, client, D, premission, color, cd)
-        }else if(fs.existsSync(`./command/general/${command}` + ".js") == true){
+            if (fs.existsSync(`./command/general/${command}` + ".js") == true) {
+                require(`../command/general/${cmd}`).execute(msg, command, args, client, D, premission, color, cd)
+            }
+        } else if (fs.existsSync(`./command/general/${command}` + ".js") == true) {
             cmd = command
             require(`../command/general/${cmd}`).execute(msg, command, args, client, D, premission, color, cd)
         }
