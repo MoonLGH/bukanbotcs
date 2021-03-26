5cff72ad24688c0b7c78614aeaa091222b3fd29f
MoonLGH <MoonLMC1212@gmail.com>
2021-03-26 17:05:42 +0700

    cooldown

--- command/general/image.js
+++ command/general/image.js
@@ -3,9 +3,19 @@ let cool
 exports.execute = async function (msg, command, args, client, D, perm, color) {
     let image = []
 
-    if(args[0] === "cooldown"){
-        if(args[1] === "true" || args[1] === "on") return cool = true
-        if(args[1] === "false" || args[1] === "off") return cool = false
+    if (perm.Sadmin || perm.host) {
+        if (args[0] === "cooldown" || args[0] == "cd") {
+            if (args[1] === "true" || args[1] === "on") {
+                cool = true
+                msg.channel.send(`Cooldown For ${commannd} Has Setted To true`)
+                return
+            }
+            if (args[1] === "false" || args[1] === "off") {
+                cool = false
+                msg.channel.send(`Cooldown For ${commannd} Has Setted To false`)
+                return
+            }
+        }
     }
     const axios = require("axios")
     const cheerio = require("cheerio")
@@ -32,17 +42,19 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
         .setFooter(`${index+1} / ${image.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
         .setColor(color);
 
-        if (cd.has(msg.author.id)) {
-            msg.delete({
-              timeout: 5000
-            })
-           return msg.channel.send("Cooldown For 10 second!").then(cod => cod.delete({timeout: 5000}))
-        }
+    if (cd.has(msg.author.id)) {
+        msg.delete({
+            timeout: 5000
+        })
+        return msg.channel.send("Cooldown For 10 second!").then(cod => cod.delete({
+            timeout: 5000
+        }))
+    }
 
-            cd.add(msg.author.id)
-            setTimeout(() => {
-              cd.delete(msg.author.id)
-            }, 10000)
+    cd.add(msg.author.id)
+    setTimeout(() => {
+        cd.delete(msg.author.id)
+    }, 10000)
     const embedsend = await msg.channel.send(embed)
     await embedsend.react("🗑️")
     await embedsend.react("⬅");
@@ -57,41 +69,41 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
     const forwardsFilter = (reaction, user) =>
         reaction.emoji.name === `➡` && user.id === msg.author.id;
 
-        const backwards = embedsend.createReactionCollector(backwardsFilter);
-        const deletes = embedsend.createReactionCollector(deleteFilter);
-        const forwards = embedsend.createReactionCollector(forwardsFilter);
-        
+    const backwards = embedsend.createReactionCollector(backwardsFilter);
+    const deletes = embedsend.createReactionCollector(deleteFilter);
+    const forwards = embedsend.createReactionCollector(forwardsFilter);
+
     deletes.on("collect", async f => {
         embedsend.delete();
     })
 
-    forwards.on("collect",async f =>{
-        if(index <= image.length){
+    forwards.on("collect", async f => {
+        if (index <= image.length) {
             index += 1
         }
         console.log(index)
         embed = new D.MessageEmbed()
-        .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-        .setTimestamp()
-        .setDescription(`Dari ${image[index].from}`)
-        .setImage(image[index].imageurl)
-        .setFooter(`${index+1} / ${image.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-        .setColor(color);
+            .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setTimestamp()
+            .setDescription(`Dari ${image[index].from}`)
+            .setImage(image[index].imageurl)
+            .setFooter(`${index+1} / ${image.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setColor(color);
 
         embedsend.edit(embed)
     })
-    
-    backwards.on("collect",async f =>{
-        if(index >= 1){
+
+    backwards.on("collect", async f => {
+        if (index >= 1) {
             index -= 1
         }
         embed = new D.MessageEmbed()
-        .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-        .setTimestamp()
-        .setDescription(`Dari: \n${image[index].from}`)
-        .setImage(image[index].imageurl)
-        .setFooter(`${index+1} / ${image.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-        .setColor(color);
+            .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setTimestamp()
+            .setDescription(`Dari: \n${image[index].from}`)
+            .setImage(image[index].imageurl)
+            .setFooter(`${index+1} / ${image.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setColor(color);
 
         console.log(index)
         embedsend.edit(embed)
