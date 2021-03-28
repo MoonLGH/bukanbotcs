359667a0e390081365e422fff39d53ffdea283d0
MoonLGH <MoonLMC1212@gmail.com>
2021-03-28 16:08:00 +0700

    dm allowed

--- command/general/translate.js
+++ command/general/translate.js
@@ -5,22 +5,9 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
 
 
     if (args[0] == "code-name") {
-        return msg.reply("https://github.com/MoonLGH/bukanbotcs/blob/main/command/general/langs.js \nFor List of Languages(and their code) \nOr Do " + `${config.prefix}${command} dm-code-name if you are to lazy to open github`)
+        return msg.reply("https://github.com/MoonLGH/bukanbotcs/blob/main/command/general/langs.js \nFor List of Languages(and their code)")
     }
-    if (args[0] == "dm-code-name") {
-        let embed = new D.MessageEmbed()
-            .setAuthor(msg.author.username, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-            .setFooter(msg.author.username, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-            .setColor(color)
-            .addField("Language", "Language name", true)
-            .addField("CodeName", "Codename Of languages", true)
 
-        for (const [key, value] of Object.entries(lang.list)) {
-            embed.addField(value, key, true)
-        }
-
-        return msg.author.send(embed)
-    }
     if (!args[0]) return msg.reply(`Do ${config.prefix}${command} [From Language] [To Language] "What you want to translate"`)
     let from
     if (lang.isSupported(args[0]) == false) return msg.reply("No Language Founded By That Type\nTry Doing " + `${config.prefix}${command} code-name`)
--- ext_module/premission.js
+++ ext_module/premission.js
@@ -1,14 +1,9 @@
 let config = require("../config.json")
 let color = "#RANDOM"
 exports.premission = function(msg,D){
-    let hostmsg , Sadmin , host
-    if(msg.member.roles.cache.get(config.superadmin)) Sadmin = true
-    else Sadmin = false
 
-    if(msg.member.roles.cache.get(config.host)) host = true
-    else host = false
-
- hostmsg = new D.MessageEmbed()
+    let hostmsg
+    hostmsg = new D.MessageEmbed()
     hostmsg.setTitle('Permintaan Gagal')
     hostmsg.addField('Kamu Tidak Memiliki Role Yang Dibutuhkan!','Role yang di butuhkan = Host')
     hostmsg.addField('\u200B','\u200B')
@@ -17,6 +12,18 @@ exports.premission = function(msg,D){
     hostmsg.setTimestamp()
     hostmsg.setFooter('Bukan Cleansound', 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
     hostmsg.setColor(color)
+
+    if(msg.author) return {Sadmin:true,host:true,hostmsg:hostmsg}
+
+
+    let Sadmin , host
+    if(msg.member.roles.cache.get(config.superadmin)) Sadmin = true
+    else Sadmin = false
+
+    if(msg.member.roles.cache.get(config.host)) host = true
+    else host = false
+
+
 
 
     return {Sadmin:Sadmin,host:host,hostmsg:hostmsg}
