707698be337f9aaf4e20d391c27fe4eb5eee3d43
MoonLGH <MoonLMC1212@gmail.com>
2021-03-31 19:12:28 +0700

    afk

---
+++ command/general/afk.js
@@ -0,0 +1,28 @@
+let afk = {}
+exports.execute = function (msg, command, args, client, D, perm, color) {
+    let reason
+    if(!args[0]){
+        reason = false
+        msg.channel.send("Okay, " + msg.author.username + " Is Now Added To Afk List")
+    }else{
+        reason = args.join(" ")
+        msg.channel.send("Okay, " + msg.author.username + " Is Now Added To Afk List" + ` With Reason = ${reason}`)
+    }
+    afk[msg.author.id] = {reason:reason}
+}
+
+exports.check = function (msg) {
+    if (afk[msg.author.id] !== undefined) {
+        msg.channel.send(`Hey <@${msg.author.id}> I've Remove Your Afk Status!`)
+        delete afk[msg.author.id]
+    }
+
+    if (msg.mentions.users.first() && afk[msg.mentions.users.first().id] !== undefined) {
+        let id = msg.mentions.users.first().id
+        if(afk[id].reason === false){
+            msg.channel.send(`<@${msg.author.id}> , The Person that you mention is Afk, With Reason ${afk[id]}`)
+        }if(afk[id].reason !== false){
+            msg.channel.send(`<@${msg.author.id}> , The Person that you mention is Afk Right Now`)
+        }
+    }
+}
--- ext_module/sleephandler.js
+++ ext_module/sleephandler.js
@@ -1,4 +1,6 @@
 const sleepcommand = require("../command/general/sleep.js")
+const afk = require("../command/general/afk.js")
 exports.sleep = function (msg) {
     sleepcommand.check(msg)
+    afk.check(msg)
 }
