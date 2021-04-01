b03c4962f047b0c51ba6519ff6c6beb070a3f2c9
MoonLGH <MoonLMC1212@gmail.com>
2021-04-01 16:58:07 +0700

    rickroll

--- command/general/avatar.js
+++ command/general/avatar.js
@@ -1,18 +1,18 @@
 exports.execute = async function (msg, command, args, client, D, perm, color) {
     let user
-    if(!args[0]){
+    if (!args[0]) {
         user = msg.author;
-    }else{
-     user = msg.mentions.users.first() || await msg.guild.members.fetch({ query: args.join(' '), limit: 1 }).then(members => members.first().user).catch(() => null) 
+    } else {
+        user = msg.mentions.users.first() || await msg.guild.members.fetch({
+            query: args.join(' '),
+            limit: 1
+        }).then(members => members.first().user).catch(() => null)
     }
+    let useravatar = user.displayAvatarURL({dynamic: true,size: 2048,format: 'png'})
     const avatarEmbed = new D.MessageEmbed()
         .setColor(color)
         .setAuthor(user.username)
-        .setDescription(`${user.username} Avatar Url is \`${user.displayAvatarURL({dynamic: true,size: 2048,format: 'png'})}\``)
-        .setImage(user.displayAvatarURL({
-            dynamic: true,
-            size: 2048,
-            format: 'png'
-        }))
+        .setDescription(`${user.username} Avatar Url is \`${useravatar}\``)
+        .setImage(useravatar)
     msg.channel.send(avatarEmbed);
 }
