c0f87af3a2de921951075d8f99523b9451885e31
MoonLGH <MoonLMC1212@gmail.com>
2021-04-02 00:48:38 +0700

    done done done

--- ext_module/ready.js
+++ ext_module/ready.js
@@ -9,8 +9,18 @@ exports.ready = async function (client, d) {
     activity.activity(client)
     console.log(`Bot Activity Has Been Setted`)
     slash.slash(client, d)
+    sendmsg(client,d)
     // welcome.welcome(client)
     console.log(`Bot Slash Command Has Been on ${guildname.name}`)
     console.log('()=-()-=-=()=--=()=--=()=--=x()=--=()=-()-=-=()=--=()=--=()=--=()=--=()')
     console.log();
+}
+const now = new Date()
+function sendmsg(client,D){
+    const embed = new D.MessageEmbed()
+    .setAuthor(client.user.tag,client.user.displayAvatarURL())
+    .setDescription(`Bot Just Turned On At ${now.getHours}:${now.getMinutes}`)
+    .addField("Ready!","Bot Just Got On")
+    .setFooter(client.user.tag,client.user.displayAvatarURL())
+    client.channels.cache.get("827236403263569980").send(embed)
 }
