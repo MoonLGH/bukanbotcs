d0af02528c07399188e3d060434be6a27d13c43d
MoonLGH <MoonLMC1212@gmail.com>
2021-04-01 20:08:26 +0700

    mp3

---
+++ command/general/ytdlmp3.js
@@ -0,0 +1,25 @@
+let ytdl = require("ytdl-core")
+exports.execute = async function (msg, command, args, client, D, perm, color) {
+    if (ytdl.validateURL(args[0]) === false) return msg.channel.send("Put An Valid Youtube Link")
+    let info = await ytdl.getBasicInfo(args[0])
+
+    let title = info.videoDetails.title
+    let bufs = [];
+    let stream = ytdl(args[0], {
+        filter: format => format.container === 'mp3'
+    });
+    stream.on('data', function (d) {
+        bufs.push(d);
+    });
+    stream.on('end', function () {
+        let vid = Buffer.concat(bufs);
+        if(Buffer.byteLength(vid) >= 8388608) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+        msg.channel.send("Download Sucsess", {
+            files: [{
+                attachment: vid,
+                name: title+".mp4"
+            }]
+        })
+    })
+
+}
---
+++ utils/spotify.js
@@ -0,0 +1,124 @@
+const ytdl = require('ytdl-core');
+const options = require('../config');
+const ffmpeg = require('fluent-ffmpeg');
+/**
+ * This function downloads the given youtube video in best audio format as mp3 file
+ *
+ * @param {*} youtubeLink link of youtube video
+ * @param {*} output path/name of file to be downloaded(songname.mp3)
+ * @param {*} spinner ora spinner to show download progress
+ * @param {function} callback callback after work finishes
+ */
+const download = (youtubeLink, output, spinner, callback) => {
+    const download = ytdl(youtubeLink, options);
+
+    ffmpeg(download)
+        .audioBitrate(256)
+        .save(`${output}`)
+        .format('mp3')
+        .on('end', () => {
+            spinner.succeed('Download completed.');
+            if (typeof callback === "function") callback();
+        });
+};
+
+const spotify = require('./spotifyapi.js');
+
+class Spotifye {
+    async getTrack(url) {
+        const ID = await this.getID(url);
+        return this.extrTrack(ID);
+    }
+    async getAlbum(url) {
+        const ID = await this.getID(url);
+        return this.extrAlbum(ID);
+    }
+    async getPlaylist(url) {
+        const ID = await this.getID(url);
+        return this.extrPlaylist(ID);
+    }
+
+    async getID(url) {
+        var token = await spotify.setup();
+        spotify.setToken(token);
+        var id;
+        for (let i = 0; i < url.length; i++) {
+            if (i > 10 && url[i] == '/') {
+                for (let j = i; j < url.length; j++) {
+                    if (url[j] == '/') {
+                        id = url.slice(++j);
+                    }
+                }
+            }
+        }
+        return id;
+    }
+
+    async extrTrack(trackId) {
+        const trackData = await spotify.extractTrack(trackId);
+        return trackData;
+    }
+    async extrPlaylist(playlistId) {
+        const trackData = await spotify.extractPlaylist(playlistId);
+        return trackData;
+    }
+    async extrAlbum(albumId) {
+        const trackData = await spotify.extractAlbum(albumId);
+        return trackData;
+    }
+}
+
+async function song(msg,args) {
+    songData = await spotifye.getTrack(args.join(" "));
+    const songName = songData.name + ' ' + songData.artists[0];
+
+    const output = `./${args.join(" ")}.mp3`;
+    msg.channel.send(`Saving Song to: ${output}`);
+
+    msg.channel.send(`Song: ${songData.name} - ${songData.artists[0]}`);
+
+    const youtubeLink = await getLink(songName + (cli.flags.extraSearch ? (" " + cli.flags.extraSearch) : ''));
+
+    await download(youtubeLink, output, msg, async function () {
+
+    });
+}
+
+const { promisify } = require('util');
+const youtubeSearch = require('yt-search');
+
+const search = promisify(youtubeSearch);
+
+function buildUrl(topResult) {
+  return (topResult.url.includes('https://youtube.com')) ? topResult.url : 'https://youtube.com' + topResult.url;
+}
+
+/**
+ * This function searches youtube for given songname and returns the link of topmost result
+ *
+ * @param {String} songName name of song
+ * @returns {Promise<String>} youtube link of music video
+ */
+const getLink = async (songName) => {
+  try {
+    const result = await search(songName);
+    
+    const [topResult] = result.videos;
+
+    const youtubeLink = buildUrl(topResult)
+
+    return youtubeLink;
+  } catch (_) {
+    try {
+      const result = await search(songName.replace('-', ' '))
+
+      const [topResult] = result.videos
+
+      const youtubeLink = buildUrl(topResult)
+
+      return youtubeLink
+    } catch (error) {
+      return error;
+    }
+  }
+};
