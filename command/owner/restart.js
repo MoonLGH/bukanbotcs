af4a81de87deb563ad7c6cbf1000f5da6ab2bb96
MoonLGH <MoonLMC1212@gmail.com>
2021-04-06 11:51:51 +0700

    ga

--- command/owner/restart.js
+++
@@ -1,6 +0,0 @@
-exports.execute = async function (msg, command, args, client, D, perm, color) {
-    let e = new D.MessageEmbed()
-        .addDescription("The Bot Is Restarting...")
-    msg.chanenl.send(e)
-    process.exit()
-}
