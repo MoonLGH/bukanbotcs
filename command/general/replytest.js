d26366b9c87e6d3e96aca69255d5b52af099f1ae
MoonLGH <MoonLMC1212@gmail.com>
2021-04-07 18:51:39 +0700

    inline

--- command/general/replytest.js
+++ command/general/replytest.js
@@ -1,15 +1,79 @@
 exports.execute = async function (msg, command, args, client, D, perm, color) {
 
-reply(msg,args.join(" "))
+    reply(msg, args.join(" "))
 
 }
-let { APIMessage } = require("discord.js")
-function reply(msg,content, options) {
+let {
+    APIMessage
+} = require("discord.js")
+
+function reply(msg, content, options) {
     return msg.channel.send(
         content instanceof APIMessage ?
         content :
-        APIMessage.transformOptions(content, options, {
+        transformOptions(content, options, {
             replyTo: msg,
         }),
     );
+}
+
+function transformOptions(content, options, extra = {}, isWebhook = false) {
+    if (!options && typeof content === 'object' && !Array.isArray(content)) {
+        options = content;
+        content = undefined;
+    }
+
+    if (!options) {
+        options = {};
+    } else if (options instanceof MessageEmbed) {
+        return isWebhook ? {
+            content,
+            embeds: [options],
+            ...extra
+        } : {
+            content,
+            embed: options,
+            ...extra
+        };
+    } else if (options instanceof MessageAttachment) {
+        return {
+            content,
+            files: [options],
+            ...extra
+        };
+    }
+
+    if (Array.isArray(options)) {
+        const [embeds, files] = this.partitionMessageAdditions(options);
+        return isWebhook ? {
+            content,
+            embeds,
+            files,
+            ...extra
+        } : {
+            content,
+            embed: embeds[0],
+            files,
+            ...extra
+        };
+    } else if (Array.isArray(content)) {
+        const [embeds, files] = this.partitionMessageAdditions(content);
+        if (embeds.length || files.length) {
+            return isWebhook ? {
+                embeds,
+                files,
+                ...extra
+            } : {
+                embed: embeds[0],
+                files,
+                ...extra
+            };
+        }
+    }
+
+    return {
+        content,
+        ...options,
+        ...extra
+    };
 }
