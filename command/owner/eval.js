d31d185216328fa20a012523f5901cc5c444fd5e
MoonLGH <MoonLMC1212@gmail.com>
2021-04-07 21:29:14 +0700

    eval ribet version

--- command/owner/eval.js
+++ command/owner/eval.js
@@ -1,4 +1,18 @@
 exports.execute = async function (msg, command, args, client, D, perm, color) {
-    const code = args.join(" ")
-    msg.channel.send(eval(code))
+    try {
+
+        const matches = msg.content.match(/```(?:(?<lang>\S+)\n)?\s?(?<code>[^]+?)\s?```/)?.groups || msg.content.match(/```(?<code>[^]+?)\s?```/)?.groups
+        msg.channel.send(eval(matches.code))
+    } catch (error) {
+        const embed = new D.MessageEmbed()
+            .setAuthor(client.user.tag, client.user.displayAvatarURL())
+            .addField("Error Occured!", error)
+            .addField("Error Stack", error.stack)
+            .addField("Error Destination", error.dest)
+            .addField("Error Line", error.lineNumber)
+            .addField("Error Column Number", error.columnNumber)
+            .setFooter(client.user.tag, client.user.displayAvatarURL())
+            .setTimestamp()
+        client.channels.cache.get("827236403263569980").send(embed)
+    }
 }
