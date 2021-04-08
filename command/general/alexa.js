d1175e9b0e838167372959e986cb62738e02708c
MoonLGH <MoonLMC1212@gmail.com>
2021-04-08 20:12:45 +0700

    ai

---
+++ command/general/alexa.js
@@ -0,0 +1,8 @@
+exports.execute = async function (msg, command, args, client, D, perm, color) {
+    const alexa = require('alexa-bot-api')
+    const ai = new  alexa()
+    
+    const reply = await ai.getReply(args.join(" "));
+    
+    msg.channel.semd(reply)
+}
