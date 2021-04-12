f16987526c214123518312c954b254c0c041c60d
MoonLGH <MoonLMC1212@gmail.com>
2021-04-12 19:28:43 +0700

    intents

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const D = require('djsmaster')
-const client = new D.Client({ intents: Intents.ALL })
+const client = new D.Client({ intents: D.Intents.ALL })
 const config = require('./config.json')
 const prefix = config.prefix
 const token = process.env.Token || config.token
