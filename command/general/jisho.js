08f98ee92b0eed79bb5e03619cb67eebdd0c8c6e
MoonLGH <MoonLMC1212@gmail.com>
2021-04-14 21:50:31 +0700

    jisho fix

--- command/general/jisho.js
+++ command/general/jisho.js
@@ -7,19 +7,23 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
     if (result.found == false) return msg.channel.send("No Kanji Founded With That Query")
 
     const embed = new D.MessageEmbed()
-    .addField('Taught in: ' , result.taughtIn)
-    .addField('JLPT level: ' , result.jlptLevel)
-    .addField('Newspaper frequency rank: ' , result.newspaperFrequencyRank)
-    .addField('Stroke count: ' , result.strokeCount)
-    .addField('Meaning: ' , result.meaning)
-    .addField('Kunyomi: ' , result.kunyomi.join(","))
-    .addField('Kunyomi example: ' , result.kunyomiExamples[0].example +" Example\n"+result.kunyomiExamples[0].reading +" Reading\n"+result.kunyomiExamples[0].meaning +" Meaning" )
-    .addField('Onyomi: ' , result.onyomi.join(","))
-    .addField('Onyomi example: ' , result.onyomiExamples[0].example +" Example\n"+result.onyomiExamples[0].reading +" Reading\n"+result.onyomiExamples[0].meaning +" Meaning" )
-    .addField('Radical: ' , `${result.radical.symbol} Symbol \n ${result.radical.forms} Forms \n ${result.radical.meaning}`)
-    .addField('Parts: ' , result.parts.join(","))
-    .setImage(result.strokeOrderGifUri)
-    .addField('Jisho Uri: ' , result.uri)
+        .addField('Taught in: ', result.taughtIn)
+        .addField('JLPT level: ', result.jlptLevel)
+        .addField('Newspaper frequency rank: ', result.newspaperFrequencyRank)
+        .addField('Stroke count: ', result.strokeCount)
+        .addField('Meaning: ', result.meaning)
+        .addField('Kunyomi: ', result.kunyomi.join(","))
+    if (result.kunyomiExamples[0]) {
+        embed.addField('Kunyomi example: ', result.kunyomiExamples[0].example + " Example\n" + result.kunyomiExamples[0].reading + " Reading\n" + result.kunyomiExamples[0].meaning + " Meaning")
+    }
+    embed.addField('Onyomi: ', result.onyomi.join(","))
+    if (result.onyomiExamples[0]) {
+        embed.addField('Onyomi example: ', result.onyomiExamples[0].example + " Example\n" + result.onyomiExamples[0].reading + " Reading\n" + result.onyomiExamples[0].meaning + " Meaning")
+    }
+    embed.addField('Radical: ', `${result.radical.symbol} Symbol \n ${result.radical.forms} Forms \n ${result.radical.meaning}`)
+        .addField('Parts: ', result.parts.join(","))
+        .setImage(result.strokeOrderGifUri)
+        .addField('Jisho Uri: ', result.uri)
 
     msg.channel.send(embed)
 }
