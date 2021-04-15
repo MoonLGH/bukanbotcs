0db3a7d84f367d99502ffb7b351c9a88ce5cf37d
MoonLGH <MoonLMC1212@gmail.com>
2021-04-15 22:27:31 +0700

    getanime

---
+++ command/general/getanime.js
@@ -0,0 +1,28 @@
+const malScraper = require('mal-scraper');
+
+exports.execute = async function (msg, command, args, client, D, perm, color) {
+    const result = await malScraper.getInfoFromName("strike")
+    const title = result.title
+    const synopsis = result.synopsis
+    const image = result.picture
+    const japaneseTitle = result.japaneseTitle
+    const synonyms = result.synonyms.join(",")
+    const genres = result.genres.join(",")
+    const studios = result.studios.join("/")
+    const episodes = result.episodes
+    const type = result.type
+
+    if(!result) return msg.channel.send("No Anime Founded")
+
+    const embed = new D.MessageEmbed()
+    .setTitle(`${title}`)
+    .addField("Japanese Title",`${japaneseTitle}`)
+    .addField("synonyms",`${synonyms}`)
+    .addField("synopsis",`${synopsis}`)
+    .addField("Episodes",`${episodes}`)
+    .addField("Genres",`${genres}`)
+    .addField("Studios",`${studios}`)
+    .addField("Type",`${type}`)
+    .setImage(image)
+    msg.channel.send(embed)
+}
--- ext_module/tags.js
+++ ext_module/tags.js
@@ -10,7 +10,7 @@ exports.execute = function (msg, command, args, client, D) {
         }
         return
     } else if ((command == "tags" || command == "tag") && (args[0] === "delete" || args[0] === "remove" || args[0] === "del")) {
-        if (!tags[args[args[1]]]) {
+        if (!tags[args[1]]) {
             return msg.channel.send("No Tags Founded With" + args[1] + "Query")
         }
         delete tags[args[1]]
