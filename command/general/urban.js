9e22df7f5e048b986d58f414045983b5f0eaa13f
MoonLGH <MoonLMC1212@gmail.com>
2021-04-17 19:52:01 +0700

    ud

---
+++ command/general/urban.js
@@ -0,0 +1,67 @@
+exports.execute = async function (msg, command, args, client, D, perm, color) {
+    const ud = require('urban-dictionary')
+
+    let index = 0
+    ud.autocompleteExtra(args.join(" ")).then((results) => {
+        let embed = new D.MessageEmbed()
+            .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setTimestamp()
+            .addField(results.term, result.preview)
+            .setFooter(`${index+1} / ${result.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setColor(color);
+        const embedsend = await msg.channel.send(embed)
+        await embedsend.react("🗑️")
+        await embedsend.react("⬅");
+        await embedsend.react("➡");
+
+        const deleteFilter = (reaction, user) =>
+            reaction.emoji.name === `🗑️` && user.id === msg.author.id;
+
+        const backwardsFilter = (reaction, user) =>
+            reaction.emoji.name === `⬅` && user.id === msg.author.id;
+
+        const forwardsFilter = (reaction, user) =>
+            reaction.emoji.name === `➡` && user.id === msg.author.id;
+
+        const backwards = embedsend.createReactionCollector(backwardsFilter);
+        const deletes = embedsend.createReactionCollector(deleteFilter);
+        const forwards = embedsend.createReactionCollector(forwardsFilter);
+
+        deletes.on("collect", async f => {
+            embedsend.delete();
+        })
+
+        forwards.on("collect", async f => {
+            if (index + 1 <= kanjis.length) {
+                index += 1
+            }
+            embed = new D.MessageEmbed()
+            embed = new D.MessageEmbed()
+                .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+                .setTimestamp()
+                .addField(results.term, result.preview)
+                .setFooter(`${index+1} / ${result.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+                .setColor(color);
+            embedsend.edit(embed)
+        })
+
+        backwards.on("collect", async f => {
+            if (index >= 1) {
+                index -= 1
+            }
+            embed = new D.MessageEmbed()
+                .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+                .setTimestamp()
+                .addField(results.term, result.preview)
+                .setFooter(`${index+1} / ${result.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+                .setColor(color);
+
+            embedsend.edit(embed)
+        })
+    }).catch((error) => {
+        const embed = new D.MessageEmbed()
+            .addField(`Error Occured - ${error.message}`)
+        return msg.channel.send(embed)
+    })
+
+}
