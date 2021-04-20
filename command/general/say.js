0692b07d2dde63a5d202953af0508187e5e66f71
MoonLGH <MoonLMC1212@gmail.com>
2021-04-20 22:12:33 +0700

    yeaa

--- command/general/say.js
+++ command/general/say.js
@@ -1,3 +1,17 @@
 exports.execute = function (msg, command, args, client, D, perm, color) {
-msg.channel.send(`${args.join(" ")}`)
+    if(!args) return;
+
+    let newargs = []
+    let delthis = false
+    args.forEach(str => {
+        if (str === "-d" || str == "-del") {
+            return delthis = true
+        }
+        newargs.push(str)
+    });
+    if(delthis == true){
+        msg.channel.send(`${newargs.join(" ")}`)
+    }else{
+        msg.channel.send(`${newargs.join(" ")}`)
+    }
 }
