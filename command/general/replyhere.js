16a344c3819f3f525bf415fb6ed6cfb2d902f73d
MoonLGH <MoonLMC1212@gmail.com>
2021-04-20 21:46:59 +0700

    awaited

--- command/general/replyhere.js
+++ command/general/replyhere.js
@@ -1,6 +1,6 @@
 exports.execute = async function (msg, command, args, client, D, perm, color) {
     if (msg.type === "REPLY") {
-        const to = msg.channel.messages.fetch(msg.reference.messageID)
+        const to = await msg.channel.messages.fetch(msg.reference.messageID)
         const contents = args.join(" ")
         replytomsg(to,contents)
     }
