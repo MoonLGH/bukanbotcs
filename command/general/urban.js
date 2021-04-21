06098318b4ae4b1249b76f5735b3ae511df2f6ad
MoonLGH <MoonLMC1212@gmail.com>
2021-04-22 00:00:39 +0700

    UD BETTER

--- command/general/urban.js
+++ command/general/urban.js
@@ -2,65 +2,74 @@ exports.execute = async function (msg, command, args, client, D, perm, color) {
     const ud = require('urban-dictionary')
 
     let index = 0
-    ud.autocompleteExtra(args.join(" ")).then(async(results) => {
-        let embed = new D.MessageEmbed()
-            .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-            .setTimestamp()
-            .addField(results[index].term, results[index].preview)
-            .setFooter(`${index+1} / ${results.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-            .setColor(color);
-        const embedsend = await msg.channel.send(embed)
-        await embedsend.react("🗑️")
-        await embedsend.react("⬅");
-        await embedsend.react("➡");
 
-        const deleteFilter = (reaction, user) =>
-            reaction.emoji.name === `🗑️` && user.id === msg.author.id;
+    const result = await ud.define("idk")
+    let embed = new D.MessageEmbed()
+        .setAuthor("Urban Dictionary", 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setTimestamp()
+        .setURL(result[index].permalink)
+        .setTitle(result[index].word)
+        .addField("Definiton", result[index].definition)
+        .addField("Written On", result[index].written_on)
+        .addField("Up/Down Vote", `${result[index].thumbs_up}/${result[index].thumbs_down}`)
+        .setFooter(`${index+1} / ${result.length} - Urban Dictionary`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+        .setColor(color);
 
-        const backwardsFilter = (reaction, user) =>
-            reaction.emoji.name === `⬅` && user.id === msg.author.id;
+    const embedsend = await msg.channel.send(embed)
+    await embedsend.react("🗑️")
+    await embedsend.react("⬅");
+    await embedsend.react("➡");
 
-        const forwardsFilter = (reaction, user) =>
-            reaction.emoji.name === `➡` && user.id === msg.author.id;
+    const deleteFilter = (reaction, user) =>
+        reaction.emoji.name === `🗑️` && user.id === msg.author.id;
 
-        const backwards = embedsend.createReactionCollector(backwardsFilter);
-        const deletes = embedsend.createReactionCollector(deleteFilter);
-        const forwards = embedsend.createReactionCollector(forwardsFilter);
+    const backwardsFilter = (reaction, user) =>
+        reaction.emoji.name === `⬅` && user.id === msg.author.id;
 
-        deletes.on("collect", async f => {
-            embedsend.delete();
-        })
+    const forwardsFilter = (reaction, user) =>
+        reaction.emoji.name === `➡` && user.id === msg.author.id;
 
-        forwards.on("collect", async f => {
-            if (index + 1 <= results.length) {
-                index += 1
-            }
-            embed = new D.MessageEmbed()
-                .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-                .setTimestamp()
-                .addField(results[index].term, results[index].preview)
-                .setFooter(`${index+1} / ${results.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-                .setColor(color);
-            embedsend.edit(embed)
-        })
+    const backwards = embedsend.createReactionCollector(backwardsFilter);
+    const deletes = embedsend.createReactionCollector(deleteFilter);
+    const forwards = embedsend.createReactionCollector(forwardsFilter);
 
-        backwards.on("collect", async f => {
-            if (index >= 1) {
-                index -= 1
-            }
-            embed = new D.MessageEmbed()
-                .setAuthor(args.join(" "), 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-                .setTimestamp()
-                .addField(results[index].term, results[index].preview)
-                .setFooter(`${index+1} / ${results.length}`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
-                .setColor(color);
-
-            embedsend.edit(embed)
-        })
-    }).catch((error) => {
-        const embed = new D.MessageEmbed()
-            .addField(`Error Occured - ${error.message}`)
-        return msg.channel.send(embed)
+    deletes.on("collect", async f => {
+        embedsend.delete();
     })
 
+    forwards.on("collect", async f => {
+        if (index + 1 <= result.length) {
+            index += 1
+        }
+        embed = new D.MessageEmbed()
+            .setAuthor("Urban Dictionary", 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setTimestamp()
+            .setURL(result[index].permalink)
+            .setTitle(result[index].word)
+            .addField("Definiton", result[index].definition)
+            .addField("Written On", result[index].written_on)
+            .addField("Up/Down Vote", `${result[index].thumbs_up}/${result[index].thumbs_down}`)
+            .setFooter(`${index+1} / ${result.length} - Urban Dictionary`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setColor(color);
+
+        embedsend.edit(embed)
+    })
+
+    backwards.on("collect", async f => {
+        if (index >= 1) {
+            index -= 1
+        }
+        embed = new D.MessageEmbed()
+            .setAuthor("Urban Dictionary", 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setTimestamp()
+            .setURL(result[index].permalink)
+            .setTitle(result[index].word)
+            .addField("Definiton", result[index].definition)
+            .addField("Written On", result[index].written_on)
+            .addField("Up/Down Vote", `${result[index].thumbs_up}/${result[index].thumbs_down}`)
+            .setFooter(`${index+1} / ${result.length} - Urban Dictionary`, 'https://cdn.discordapp.com/icons/801839309073678346/99b51796e8c2da53a4813873408a4fb2.webp?size=256')
+            .setColor(color);
+
+        embedsend.edit(embed)
+    })
 }
