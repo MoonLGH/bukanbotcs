bb93ca11d4db39c17fddd819711abe2f61b91d9a
MoonLGH <MoonLMC1212@gmail.com>
2021-05-03 21:12:01 +0700

    more test

--- command/general/instadl.js
+++ command/general/instadl.js
@@ -1,20 +1,3 @@
 exports.execute = async function (msg, command, args, client, D, perm, color) {
-    const {
-        promisify
-    } = require('util')
-    const {
-        instagram,
-        twitter
-    } = require('video-url-link')
-
-    const igGetInfo = promisify(instagram.getInfo)
-
-    const result = await igGetInfo(args[0], {})
-    console.log(result)
-    // msg.channel.send("Download success ", {
-    //     files: [{
-    //         attachment: result.variants[result.variants.length - 1].url,
-    //         name: result.full_text + ".mp4"
-    //     }]
-    // })
+msg.channel.send("im workin on it, calm down")
 }
