217155ed44941dbf9a115ea82e18d4c4082c56fa
MoonLGH <MoonLMC1212@gmail.com>
2021-06-12 14:31:19 +0700

    try catch to get info if the video is region locked

--- command/general/ytdl.js
+++ command/general/ytdl.js
@@ -1,29 +1,35 @@
 let ytdl = require("ytdl-core")
 exports.execute = async function (msg, command, args, client, D, perm, color) {
     if (ytdl.validateURL(args[0]) === false) return msg.channel.send("Put An Valid Youtube Link")
-    let info = await ytdl.getBasicInfo(args[0])
-
-    let title = info.videoDetails.title
-    let bufs = [];
-    let stream = ytdl(args[0], {
-        filter: format => format.container === 'mp4'
-    });
-    stream.on('data', function (d) {
-        bufs.push(d);
-    });
-    stream.on('end', function () {
-        let vid = Buffer.concat(bufs);
-        
-        if((msg.guild.premiumTier === "NONE" || msg.guild.premiumTier === "TIER_1" || msg.channel.type === "dm")&& Buffer.byteLength(vid) >= 8388608) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
-        if(msg.guild.premiumTier === "TIER_2" && Buffer.byteLength(vid) >= 52428800) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
-        if(msg.guild.premiumTier === "TIER_3" && Buffer.byteLength(vid) >= 104857600) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
-        
-        msg.channel.send("Download success ", {
-            files: [{
-                attachment: vid,
-                name: title+".mp4"
-            }]
+    try {
+        let info = await ytdl.getBasicInfo(args[0])
+        let title = info.videoDetails.title
+        let bufs = [];
+        let stream = ytdl(args[0], {
+            filter: format => format.container === 'mp4'
+        });
+        stream.on('data', function (d) {
+            bufs.push(d);
+        });
+        stream.on('end', function () {
+            let vid = Buffer.concat(bufs);
+            
+            if((msg.guild.premiumTier === "NONE" || msg.guild.premiumTier === "TIER_1" || msg.channel.type === "dm")&& Buffer.byteLength(vid) >= 8388608) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+            if(msg.guild.premiumTier === "TIER_2" && Buffer.byteLength(vid) >= 52428800) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+            if(msg.guild.premiumTier === "TIER_3" && Buffer.byteLength(vid) >= 104857600) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+            
+            msg.channel.send("Download success ", {
+                files: [{
+                    attachment: vid,
+                    name: title+".mp4"
+                }]
+            })
         })
-    })
-
+    
+    } catch (err){
+        if (err === "MinigetError: Status code: 404"){
+            msg.channel.send("Video Is Privated Or Region Locked")
+        }
+    }
+    
 }
--- command/slash/ytdl.js
+++ command/slash/ytdl.js
@@ -18,28 +18,34 @@ let ytdl = require("ytdl-core")
 
 async function dl(msg) {
     if (ytdl.validateURL(msg.options[0].value) === false) return msg.channel.send("Put An Valid Youtube Link")
-    let info = await ytdl.getBasicInfo(msg.options[0].value)
-
-    let title = info.videoDetails.title
-    let bufs = [];
-    let stream = ytdl(msg.options[0].value, {
-        filter: format => format.container === 'mp4'
-    });
-    stream.on('data', function (d) {
-        bufs.push(d);
-    });
-    stream.on('end', function () {
-        let vid = Buffer.concat(bufs);
-        
-        if((msg.guild.premiumTier === "NONE" || msg.guild.premiumTier === "TIER_1" || msg.channel.type === "dm")&& Buffer.byteLength(vid) >= 8388608) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
-        if(msg.guild.premiumTier === "TIER_2" && Buffer.byteLength(vid) >= 52428800) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
-        if(msg.guild.premiumTier === "TIER_3" && Buffer.byteLength(vid) >= 104857600) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
-        
-        msg.channel.send("Download success ", {
-            files: [{
-                attachment: vid,
-                name: title+".mp4"
-            }]
+    try {
+        let info = await ytdl.getBasicInfo(msg.options[0].value)
+        let title = info.videoDetails.title
+        let bufs = [];
+        let stream = ytdl(msg.options[0].value, {
+            filter: format => format.container === 'mp4'
+        });
+        stream.on('data', function (d) {
+            bufs.push(d);
+        });
+        stream.on('end', function () {
+            let vid = Buffer.concat(bufs);
+            
+            if((msg.guild.premiumTier === "NONE" || msg.guild.premiumTier === "TIER_1" || msg.channel.type === "dm")&& Buffer.byteLength(vid) >= 8388608) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+            if(msg.guild.premiumTier === "TIER_2" && Buffer.byteLength(vid) >= 52428800) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+            if(msg.guild.premiumTier === "TIER_3" && Buffer.byteLength(vid) >= 104857600) return msg.channel.send(`The Video Is Too Big Try Download It Yourself in \n https://ssyoutube.com/`)
+            
+            msg.channel.send("Download success ", {
+                files: [{
+                    attachment: vid,
+                    name: title+".mp4"
+                }]
+            })
         })
-    })
+    
+    } catch (err){
+        if (err === "MinigetError: Status code: 404"){
+            msg.channel.send("Video Is Privated Or Region Locked")
+        }
+    }
 }
