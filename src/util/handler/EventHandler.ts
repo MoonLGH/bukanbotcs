a5712e667a4ee3860dc4ce90d5f000509579494b
MoonLGH <MoonLMC1212@gmail.com>
2021-10-16 19:15:43 +0700

    yeet event file

--- src/util/handler/EventHandler.ts
+++ src/util/handler/EventHandler.ts
@@ -1,18 +1,16 @@
-import fs from "fs";
+const fs = require("fs")
 import { Client } from "discord.js";
 export function setup(client: Client) {
 	const eventFiles = fs
-		.readdirSync("./src")
-		// .filter((file:string) => file.endsWith(".ts"));
+		.readdirSync("./src/events/")
+		.filter((file:string) => file.endsWith(".ts"));
 
-		console.log("Event Handle Started")
 	for (const file of eventFiles) {
-		console.log(file)
-		// const event = require(`../../Events/${file}`);
-		// if (event.once) {
-		// 	client.once(event.name, (...args: any) => event.execute(...args, client));
-		// } else {
-		// 	client.on(event.name, (...args: any) => event.execute(...args, client));
-		// }
+		const event = require(`../../Events/${file}`);
+		if (event.once) {
+			client.once(event.name, (...args: any) => event.execute(...args, client));
+		} else {
+			client.on(event.name, (...args: any) => event.execute(...args, client));
+		}
 	}
 }
